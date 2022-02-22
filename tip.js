8e082c71eb87ca20db3ff16e39f618f93954de9d
Adithya <jhadith88@gmail.com>
2022-02-22 16:52:49 +0530

    first commit

---
+++ tip.js
@@ -0,0 +1,37 @@
+const web3 = new Web3(Web3.givenProvider)
+
+const form = document.querySelector('form');
+
+const send = async(amount) =>{
+   const accounts= await window.ethereum.request({method:"eth_requestAccounts"}) //after this line execution only the next line execution will start
+//    alert(accounts)
+    const wei = web3.utils.toWei(amount,"ether")
+    if(accounts.length>0){
+        window.ethereum.request({
+            method: "eth_sendTransaction",
+            params: [{
+                from: accounts[0],
+                to:"0x3FDE4bCd2FAEe79e7D99Be1E693193CEEc998BE9", //mainnet 
+                // to:"0x5D89B94f49f954322fFbAf04EFdC4Fd32564B4f0", //ganache test account
+                value: web3.utils.toHex(wei)
+            }]
+        })
+    }
+}
+if(window.ethereum){
+    form.classList.add('has-eth') 
+
+}
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+    
+    if(window.ethereum){
+        const input = form.querySelector("input")
+        send(input.value) // this function runs when user clicks the form and he/she have wallet in the browser!
+
+    }else{
+        alert("Please install the wallet")
+    }
+
+})
