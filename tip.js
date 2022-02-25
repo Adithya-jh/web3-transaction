7447d22e0b50997277102a86753983dc8ba4628f
Adithya <jhadith88@gmail.com>
2022-02-25 13:22:39 +0530

    updated auth

--- tip.js
+++ tip.js
@@ -18,9 +18,53 @@ const send = async(amount) =>{
         })
     }
 }
+
+const button = document.querySelector(".button")
+
+let accounts =[]
+
+const connect = async() =>{
+     accounts= await window.ethereum.request({method:"eth_requestAccounts"})
+    // const wei = web3.utils.toWei(amount,"ether")
+    console.log(accounts[0]);
+    if(accounts.length>0){
+        window.ethereum.request({
+            method: "eth_requestAccounts",
+            // params: [{
+            //     from: accounts[0],
+            //     to:"0x3FDE4bCd2FAEe79e7D99Be1E693193CEEc998BE9", //mainnet 
+            //     // to:"0x5D89B94f49f954322fFbAf04EFdC4Fd32564B4f0", //ganache test account
+            //     // value: web3.utils.toHex(wei)
+            // }]
+        })
+    }
+
+    if(accounts.length>0){
+        document.getElementById("btn").textContent = accounts[0]
+    }else{
+        document.getElementById("btn").textContent ="Connect To Wallet"
+    }
+
+}
+
+
+
+
+
+
+button.addEventListener("click",(e)=>{
+    e.preventDefault()
+    if(window.ethereum){
+        connect()
+    }
+    // console.log("hello");
+})
+
+
+
+
 if(window.ethereum){
     form.classList.add('has-eth') 
-
 }
 
 form.addEventListener('submit', function(event){
@@ -35,3 +79,4 @@ form.addEventListener('submit', function(event){
     }
 
 })
+
